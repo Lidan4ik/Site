efbb0beb4f61dbc2ab712431aedc8543ea855ec0
Daniil Ovsyannikov <123@gmail.com>
2022-05-12 09:54:11 -0700

    Regisration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   resources :humen
   #get 'home/index'
   root 'home#index'
   get 'home/about'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
